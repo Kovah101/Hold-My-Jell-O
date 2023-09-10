bb4df610ae0a1636bc0e75fc8da24a87444a6791
Elliot Thompson <elliotthompson101@gmail.com>
2023-09-10 15:51:53 -0700

    Added player hand and movement

---
+++ Assets/Scripts/GameEventSystem.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameEventSystem : MonoBehaviour
+{ 
+    public static GameEventSystem Instance { get; private set; }
+
+    public BasicGameEvent StartGameEvent = new BasicGameEvent();
+    public BasicGameEvent ResetGameEvent = new BasicGameEvent();
+    public BasicGameEvent FinishGameEvent = new BasicGameEvent();
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Debug.LogWarning("Duplicate NodeEventSystem instance found. Removing duplicate.");
+            Destroy(gameObject);
+        }
+    }
+}
+
+[System.Serializable]
+public class BasicGameEvent : UnityEvent { }
