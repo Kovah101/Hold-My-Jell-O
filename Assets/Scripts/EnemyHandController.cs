c8426ce942329e9eef483c4930ef7a47ce591bd2
Elliot Thompson <elliotthompson101@gmail.com>
2023-09-29 18:12:06 -0700

    added enemy hand movement and destruction

--- Assets/Scripts/EnemyHandController.cs
+++ Assets/Scripts/EnemyHandController.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class EnemyHandController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float moveSpeed = 5f;
+
+    private bool finished = false;
+    public float deadZone = -7.5f;
+
     void Start()
     {
-        
+        GameEventSystem.Instance.FinishGameEvent.AddListener(OnFinishGame);
+        GameEventSystem.Instance.ResetGameEvent.AddListener(OnResetGame);
+
     }
 
-    // Update is called once per frame
+    private void OnDisable()
+    {
+        GameEventSystem.Instance.FinishGameEvent.RemoveListener(OnFinishGame);
+        GameEventSystem.Instance.ResetGameEvent.RemoveListener(OnResetGame);
+    }
+
     void Update()
     {
-        
+       if (finished == false)
+        {
+            transform.position += Vector3.down * moveSpeed * Time.deltaTime;
+        }
+
+       if (transform.position.y < deadZone)
+        {
+            Destroy(gameObject);
+        }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Jelly")
+        {
+            GameEventSystem.Instance.FinishGameEvent.Invoke();
+        }
+    }
+
+    private void OnFinishGame()
+    {
+        finished = true;
+    }
+
+    private void OnResetGame()
+    {
+        Destroy(gameObject);
+    }
+    
 }
