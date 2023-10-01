74e7321bcdd32a7d1473ee63f63077e8e6dc1139
Elliot Thompson <elliotthompson101@gmail.com>
2023-10-01 11:31:22 -0700

    live score and final score working as expected

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,12 +9,19 @@ public class Manager : MonoBehaviour
 {
     public GameObject gameOverMenu;
     public GameObject gameStartScreen;
+    public TMP_Text liveScore;
+    public TMP_Text finalScore;
+    public double scoreMultiplier = 1;
 
-    // Start is called before the first frame update
+    private double startTime;
+    private double currentTime;
+    private double score;
+
     void Start()
     {
         GameEventSystem.Instance.StartGameEvent.AddListener(OnStartGame);
         GameEventSystem.Instance.FinishGameEvent.AddListener(OnFinishGame);
+        GameEventSystem.Instance.ResetGameEvent.AddListener(OnResetGame);
 
     }
 
@@ -20,18 +29,39 @@ public class Manager : MonoBehaviour
     {
         GameEventSystem.Instance.StartGameEvent.RemoveListener(OnStartGame);
         GameEventSystem.Instance.FinishGameEvent.RemoveListener(OnFinishGame);
+        GameEventSystem.Instance.ResetGameEvent.RemoveListener(OnResetGame);
+    }
+
+    void Update()
+    {
+        if (gameStartScreen.activeSelf == false && gameOverMenu.activeSelf == false)
+        {
+            currentTime = System.Environment.TickCount;
+            score = (currentTime - startTime) * scoreMultiplier;
+           // score += Time.deltaTime * scoreMultiplier; // score in multiples of seconds
+            liveScore.text = score.ToString("F0");
+        }
     }
 
     private void OnStartGame()
     {
         Debug.Log("Game Started");
         gameStartScreen.SetActive(false);
+        startTime = System.Environment.TickCount;
     }
 
     private void OnFinishGame()
     {
         Debug.Log("Game Finished - setting game over menu to active");
         gameOverMenu.SetActive(true);
+        finalScore.text = "Score: " + score.ToString("F0");
+    }
+
+    private void OnResetGame()
+    {
+        Debug.Log("Game Reset");
+        liveScore.text = "0";
+        
     }
 
     public void ReturnToMenu()
