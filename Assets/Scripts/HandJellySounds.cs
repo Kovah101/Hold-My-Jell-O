e2e906c5cecf2d5c09d756d522f8a0c81b178959
Elliot Thompson <elliotthompson101@gmail.com>
2023-10-02 12:28:11 -0700

    jelly sounds working on hand and enemies

---
+++ Assets/Scripts/HandJellySounds.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandJellySounds : MonoBehaviour
+{
+    public AudioSource[] jellysplats;
+
+    private float timer = 0f;
+    private float maxTimer = 1.5f;
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if(gameObject.CompareTag("Player"))
+        {
+           if(timer > maxTimer)
+            {
+                PlayJellySplat(collision);
+                timer = 0f;
+            }
+
+        } 
+        else if (gameObject.CompareTag("Enemy"))
+        {
+            PlayJellySplat(collision);
+        }
+
+        
+    }
+
+    private void PlayJellySplat(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Jelly"))
+        {
+            jellysplats[Random.Range(0, jellysplats.Length)].Play();
+        }
+    }
+
+    void Update()
+    {
+        if(gameObject.CompareTag("Player") && timer < maxTimer)
+        {
+            Debug.Log(timer);
+            timer += Time.deltaTime;
+        }
+    }
+    
+}
