95f42fd0285fc17a1448737e4ca2f3368f2eb0ab
Elliot Thompson <elliotthompson101@gmail.com>
2023-10-03 12:13:24 -0700

    test variables ui complete, starting scripting

---
+++ Assets/Scripts/TestVariableController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestVariableController : MonoBehaviour
+{
+    public Slider scoreDifficulty;
+    public TMP_Text scoreDifficultyText;
+    public Slider speedDifficulty;
+    public TMP_Text speedDifficultyText;
+    public Slider timerDifficulty;
+    public TMP_Text timerDifficultyText;
+    public Slider spawnDifficulty;
+    public TMP_Text spawnDifficultyText;
+    public Slider startingSpeed;
+    public TMP_Text startingSpeedText;
+    public Slider speedIncrement;
+    public TMP_Text speedIncrementText;
+    public Slider startingSpawnTimer;
+    public TMP_Text startingSpawnTimerText;
+    public Slider spawnTimerDecrement;
+    public TMP_Text spawnTimerDecrementText;
+    public Slider spawnPatternIncrement;
+    public TMP_Text spawnPatternIncrementText;
+    public Toggle invincibility;
+    public TMP_Text invincibilityText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
